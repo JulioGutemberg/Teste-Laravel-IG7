38f570170eb5e96ed195089ba0b68613f08ac333
Júlio Gutemberg Carvalho de Sousa <juliogutemberg.dev@gmail.com>
2022-11-25 23:39:37 -0300

    feat: atualiza Layouts

--- sipae/resources/views/components/text-input.blade.php
+++ sipae/resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-green-700 focus:ring-green-700 rounded-md shadow-sm']) !!}>
--- sipae/resources/views/layouts/app.blade.php
+++ sipae/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'SIPAE') }}</title>
+        <title>{{ 'SIPAE - Admin'}}</title><link src="imagens/SIPAE-ICON.png" >
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
