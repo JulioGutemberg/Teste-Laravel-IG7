6582e9ae6aa58e63bb3e33768dd244897bb3cc68
Júlio Gutemberg Carvalho de Sousa <juliogutemberg.dev@gmail.com>
2022-11-28 22:09:22 -0300

    fix:  correção de layout

--- sipae/resources/views/layouts/app.blade.php
+++ sipae/resources/views/layouts/app.blade.php
@@ -5,8 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ 'SIPAE - Admin'}}</title><link src="/Imagens/SIPAE-ICON.png" >
-
+        <title>{{ 'SIPAE - Admin'}}</title>
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
--- sipae/routes/web.php
+++ sipae/routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\EscolasController;
 use App\Http\Controllers\ProfileController;
-use Database\Seeders\userAdmin;
 use Illuminate\Support\Facades\Route;
-use Spatie\Permission\Models\Role;
+
 
 /*
 |--------------------------------------------------------------------------
